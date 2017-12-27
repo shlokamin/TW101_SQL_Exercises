1e232bbc93a08d03be7cbad3c0ec44f427aafddc
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-27 15:52:00 -0800

    deleted the .sql files in root directory

--- ex3.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO person (id, first_name, last_name, age) VALUES (0, 'Zed', 'Shaw', 37);
-INSERT INTO person (id, first_name, last_name, age) VALUES (1, 'Shlok', 'Amin', 23);
-INSERT INTO pet (id, name, breed, age, dead, person_id)
-    VALUES (0, 'Fluffy', 'Unicorn', 1000, 0, 0);
-INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1, 0);
-INSERT INTO pet (id, name, breed, age, dead, person_id)
-    VALUES (2, 'Mittens', 'cat', 12, 0, 1);
-
--- ex4.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
-INSERT INTO person_pet (person_id, pet_id) VALUES (0, 1);
-INSERT INTO person_pet (person_id, pet_id) VALUES (1, 2);
-
