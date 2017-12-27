9957894a818ae1c061bf147a0c70268090a55ac4
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-26 21:25:21 -0800

    finished exercises 1-5

---
+++ ex3.sql
@@ -0,0 +1,8 @@
+INSERT INTO person (id, first_name, last_name, age) VALUES (0, 'Zed', 'Shaw', 37);
+INSERT INTO person (id, first_name, last_name, age) VALUES (1, 'Shlok', 'Amin', 23);
+INSERT INTO pet (id, name, breed, age, dead, person_id)
+    VALUES (0, 'Fluffy', 'Unicorn', 1000, 0, NULL);
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1, 0);
+INSERT INTO pet (id, name, breed, age, dead, person_id)
+    VALUES (2, 'Mittens', 'cat', 12, 1, 1);
+
---
+++ ex4.sql
@@ -0,0 +1,3 @@
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 1);
+INSERT INTO person_pet (person_id, pet_id) VALUES (1, 2);
+
